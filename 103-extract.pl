1384e0438c3775f00d93097959a8cb8f6821949f
MacCampbell <campbellmatthewa@gmail.com>
2021-06-02 13:28:49 -0800

    Forgot subroutine

--- 103-extract.pl
+++ 103-extract.pl
@@ -39,7 +39,7 @@ foreach my $site (@sites) {
 	my $minor=$b[3];
 	my $maf=$b[4];
 
-`samtools faidx /home/maccamp/genomes/hypomesus-20210204/Hyp_tra_F_20210204.fa	> temp.fasta`;
+`samtools faidx /home/maccamp/genomes/hypomesus-20210204/Hyp_tra_F_20210204.fa $chrom	> temp.fasta`;
 
 my $fasta = "temp.fasta";
 
@@ -75,4 +75,46 @@ while (<INFILE>) {
 
 return(@result);
 
+}
+
+
+sub ReadInFASTA {
+    my $infile = shift;
+    my @line;
+    my $i = -1;
+    my @result = ();
+    my @seqName = ();
+    my @seqDat = ();
+
+    open (INFILE, "<$infile") || die "Can't open $infile\n";
+
+    while (<INFILE>) {
+        chomp;
+        if (/^>/) {  # name line in fasta format
+            $i++;
+            s/^>\s*//; s/^\s+//; s/\s+$//;
+            $seqName[$i] = $_;
+            $seqDat[$i] = "";
+        } else {
+            s/^\s+//; s/\s+$//;
+	    s/\s+//g;                  # get rid of any spaces
+            next if (/^$/);            # skip empty line
+            s/[uU]/T/g;                  # change U to T
+            $seqDat[$i] = $seqDat[$i] . uc($_);
+        }
+
+	# checking no occurence of internal separator $sep.
+	die ("ERROR: \"$sep\" is an internal separator.  Line $. of " .
+	     "the input FASTA file contains this charcter. Make sure this " . 
+	     "separator character is not used in your data file or modify " .
+	     "variable \$sep in this script to some other character.\n")
+	    if (/$sep/);
+
+    }
+    close(INFILE);
+
+    foreach my $i (0..$#seqName) {
+	$result[$i] = $seqName[$i] . $sep . $seqDat[$i];
+    }
+    return (@result);
 }
